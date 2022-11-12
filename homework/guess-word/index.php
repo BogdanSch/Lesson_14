cbce2992c7c90e35971586317e182ca0536e78c0
Bohdan <bogmira777@gmail.com>
2022-11-12 13:18:48 +0100

    Fix guess a word task

--- homework/guess-word/index.php
+++ homework/guess-word/index.php
@@ -4,6 +4,7 @@ $word = "exploration";
 
 $count = 0;
 $remain = strlen($word) - 2;
+$wrong_try = 0;
 $answer = [];
 $answer[0] = $word[0];
 for ($i = 1; $i < strlen($word) - 1; $i++) { 
@@ -16,12 +17,18 @@ if(isset($_POST['Submit'])){
     $letter = $_POST['letter'];
     $answer = explode(",", $_POST['answer']);
     $remain = $_POST['remain'];
+    $wrong_try = $_POST['try'];
+    $correct = false;
 
     for ($i = 1; $i < strlen($word) - 1; $i++) { 
-        if($word[$i] === $letter){
+        if($word[$i] === $letter && $answer[$i] != $letter){
             $answer[$i] = $letter;
-            $remain--;
+            $remain -= 1;
+            $correct = true;
         }
+    }
+    if(!$correct){
+        $wrong_try += 1;
     }
 }
 ?>
@@ -43,18 +50,24 @@ if(isset($_POST['Submit'])){
         }
         print "</p>"
     ?>
-    <?= 'Remains: '.$remain ?>
+    <?= 'Remains: '.$remain ?><br>
+    <?= 'Wrong trys: '.$wrong_try ?>
     <?php
-         if($remain == 0){
+        if($remain === 0){
             echo "<h1>You won!</h1>";
         }
-        echo "<h3>$count| Move</h3>"
+        if($wrong_try === 4){
+            echo "<h1>You lost!</h1>";
+        }
+        if($count != 0)
+            echo "<h3>$count| Move</h3>"
     ?>
     <form action="index.php" method="POST">
         <label for="letter">Enter the letter: 
             <input type="text" name="letter">
             <input type="hidden" name="answer" value="<?= implode(",", $answer) ?>">
             <input type="hidden" name="remain" value="<?= $remain ?>">
+            <input type="hidden" name="try" value="<?= $wrong_try ?>">
             <input type="hidden" name="count" value="<?= $count ?>">
         </label>
         <input type="submit" name="Submit" value="Check">
